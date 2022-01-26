2968422d0d4052d2c273e6893cd591ab70277122
Thanos Delatolas <thanos.delatolas98@gmail.com>
2022-01-26 15:56:03 +0200

    neural net is ready

--- matlab/real_data_cnn_input.m
+++ matlab/real_data_cnn_input.m
@@ -33,7 +33,8 @@ load(sprintf('../real_data/%sms/eeg_topo_real_yi_%sms.mat',ms,ms));
 
 load('../duneuropy/Data/dipoles_downsampled_10k.mat')
 
-pred = readNPY('../real_data/25_8ms/pred_sources_25_8.npy');
+%pred = readNPY('../real_data/25_8ms/pred_sources_25_8.npy');
+pred = readNPY('../../../Downloads/pred_sources_real.npy');
 
 
 
